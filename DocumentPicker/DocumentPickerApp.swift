f8ab75f8527088bfde28ec360e332108d7731667
Alexander Römer <alex@roemer.design>
2020-09-26 19:54:04 +0200

    Initial Commit

---
+++ DocumentPicker/DocumentPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentPickerApp.swift
+//  DocumentPicker
+//
+//  Created by Alexander Römer on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct DocumentPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
